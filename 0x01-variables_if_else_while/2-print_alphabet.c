baa20fd868ec845e9beec33d1477df524ec2a29c
Younes <essayidyounes8@gmail.com>
2023-02-17 09:17:57 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,13 +5,13 @@
  * Return: Always (Success)
  */
 
- int main(void)
- {
- char c;
+int main(void)
+{
+char c;
 
- for (c = 'a'; c <= 'z'; c++)
- putchar(c);
- putchar('\n');
- return (0);
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
  
- }
+}
